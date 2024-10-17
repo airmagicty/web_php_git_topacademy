9d4f7dc39aa1f5db90593cb29f5824877934e1ab
mahfoudh <mahfoodh20015@gmail.com>
2024-10-17 11:46:26 +0300

    conn

--- mahfoudh/includes/connect.php
+++ mahfoudh/includes/connect.php
@@ -9,7 +9,7 @@ try {
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
-         die("Connection failed: " . $conn->connect_error);
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
 } catch (Exception $e) {
     error_log($e->getMessage());
