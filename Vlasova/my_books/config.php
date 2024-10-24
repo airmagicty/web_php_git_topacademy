bf4765e4a8f79873e6307bc7a9f2b872eee65dbf
Anna <aniuta1784@gmail.com>
2024-10-24 18:39:35 +0300

    Чистка мусора

--- Vlasova/my_books/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DB_HOST = 'localhost';
-$DB_USEWR = 'root';
-$DB_PASS ='';
-$DB_NAME = 'books';
-;
-?>
--- Vlasova/readers2/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect ("localhost","root","","books"); 
-if(!$con) 
-{ 
-  die("cannot connect to server"); 
-}       
-?>
